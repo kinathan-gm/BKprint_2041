1fbc7cc9e34ad0bac8a4ecb950809c2581802973
tuc <tucgiakhuong@gmail.com>
2024-12-01 19:32:50 +0700

    quan ly may in va config

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PrinterController;
+use App\Http\Controllers\PrinterConfigurationController;
 
 
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/printers', [PrinterController::class, 'getPrinters']);
+Route::post('/printers', [PrinterController::class, 'store']);
+Route::put('/printers/{id}', [PrinterController::class, 'update']);
+Route::get('/adminPrinters', [PrinterController::class, 'getAdminPrinters']);
 
+Route::get('/printer-config', [PrinterConfigurationController::class, 'getConfig']);
+Route::put('/update-num-pages', [PrinterConfigurationController::class, 'updateNumPages']);
+Route::put('/update-config-date', [PrinterConfigurationController::class, 'updateConfigDate']);
+Route::put('/update-allowed-file-types', [PrinterConfigurationController::class, 'updateAllowedFileTypes']);
